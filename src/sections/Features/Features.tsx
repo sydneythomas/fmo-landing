ef970f38fab5933a56d5cc34cee487e5584ab866
Sydney Allison Thomas <sydneyathomas@gmail.com>
2022-06-02 17:22:53 +0200

    change features grid layout

--- src/sections/Features/Features.tsx
+++ src/sections/Features/Features.tsx
@@ -8,11 +8,14 @@ import Drink from "../../assets/cocktail.jpeg";
 import Towel from "../../assets/hottowel.jpeg";
 import Lounge2 from "../../assets/cigar lounge.jpeg";
 import Feet from "../../assets/feet.jpeg";
+import ManAtBar from "../../assets/man_at_bar.jpeg";
 
 export default function Features() {
+  const photos = [Barber, Feet, Towel, ManAtBar];
+
   const features = [
     {
-      title: "Trained Barbers",
+      title: "Barber & Facials",
       text: "Allow our professionally trained barbers to trim & sculpt your mane to perfection. We offer razor shaves, shaping, and a variety of other techniques for all hair types.",
       img: Barber,
     },
@@ -22,43 +25,63 @@ export default function Features() {
       img: Feet,
     },
     {
-      title: "Soothing Facials",
-      text: "Pamper your skin with a luxurious hot towel facial. Lay back, relax, & allow the steam to rejuvenate your pores.",
-      img: Towel,
-    },
-    {
-      title: "The Den",
-      text: "Coming soon to our north Atlanta location, The Den is a perfect place to network or wind down while enjoying one of our topshelf cocktails, cigars, & live music.",
-      img: Lounge2,
-    },
-    {
-      title: "Fine Cigars",
-      text: "Browse our selection of cigars or enjoy one of your own in our private lounge.",
-      img: Cigar,
-    },
-    {
-      title: "Bespoke Cocktails",
+      title: "The Bar",
       text: "Every gentleman will receive a complimentary beverage with each grooming service. We offer a selection of premium, expertly-crafted cocktails.",
-      img: Drink,
+      img: ManAtBar,
     },
+    // {
+    //   title: "Soothing Facials",
+    //   text: "Pamper your skin with a luxurious hot towel facial. Lay back, relax, & allow the steam to rejuvenate your pores.",
+    //   img: Towel,
+    // },
+    // {
+    //   title: "The Den",
+    //   text: "Coming soon to our north Atlanta location, The Den is a perfect place to network or wind down while enjoying one of our topshelf cocktails, cigars, & live music.",
+    //   img: Lounge2,
+    // },
+    // {
+    //   title: "Fine Cigars",
+    //   text: "Browse our selection of cigars or enjoy one of your own in our private lounge.",
+    //   img: Cigar,
+    // },
+    // {
+    //   title: "Bespoke Cocktails",
+    //   text: "Every gentleman will receive a complimentary beverage with each grooming service. We offer a selection of premium, expertly-crafted cocktails.",
+    //   img: Drink,
+    // },
   ];
 
   return (
-    <div className="feature-grid">
-      {features.map((feature, index) => {
-        const firstPart = feature.title.split(" ").slice(0, -1).join() + " ";
-        const lastWord = feature.title.split(" ").pop();
-        return (
-          <div className="feature-card" key={index}>
-            <img src={feature.img} alt={feature.title} />
-            <p className="lora header">
-              <span className="lora header">{firstPart}</span>
-              <span className="lora header accent">{lastWord}</span>
-            </p>
-            <p>{feature.text}</p>
-          </div>
-        );
-      })}
+    <div className="feature-container">
+      <div className="photo-grid">
+        <div className="div0">
+          <p className="lora title gold">OUR SERVICES</p>
+        </div>
+
+        {photos.map((photo, index) => {
+          return (
+            <div className={`div${index + 1}`}>
+              <img src={photo} />
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="feature-grid">
+        {features.map((feature, index) => {
+          const firstPart = feature.title.split(" ").slice(0, -1).join() + " ";
+          const lastWord = feature.title.split(" ").pop();
+          return (
+            <div className="feature-card" key={index}>
+              <p className="lora header">
+                <span className="lora header">{firstPart}</span>
+                <span className="lora header accent">{lastWord}</span>
+              </p>
+              <p>{feature.text}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
